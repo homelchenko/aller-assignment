19f825bcc070e4023d862800de63ae866ff58300
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2020-05-04 15:32:05 +0300

    Run HTTP server with stub (empty) news feed

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/homelchenko/aller-assignment/news"
 )
 
 func main() {
+	http.HandleFunc("/news", newsFeed)
 	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
+
+func newsFeed(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	feed := news.Feed{}
+	enc := json.NewEncoder(w)
+
+	err := enc.Encode(feed)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
