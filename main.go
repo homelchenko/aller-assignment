86327ba069efbef17e669d82adb532c05d096fa2
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2020-05-04 15:55:30 +0300

    Download upstream and produce news feed

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/homelchenko/aller-assignment/news"
+	"github.com/homelchenko/aller-assignment/news/downloader"
 )
 
 func main() {
@@ -20,7 +22,33 @@ func newsFeed(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	feed := news.Feed{}
+	articleDowloader := downloader.NewArticleFeedReader()
+	marketingDownloader := downloader.NewMarketingFeedReader()
+
+	ca := make(chan articleResult)
+	cm := make(chan marketingResult)
+
+	go downloadArticles(req.Context(), ca, articleDowloader)
+	go downloadMarketing(req.Context(), cm, marketingDownloader)
+
+	ar := <-ca
+	mr := <-cm
+
+	if ar.err != nil {
+		log.Printf("error downloading feed: %s", ar.err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	if mr.err != nil {
+		log.Printf("error downloading feed: %s", mr.err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	feed := news.ProduceNewsFeed(ar.articles, mr.marketing)
 	enc := json.NewEncoder(w)
 
 	err := enc.Encode(feed)
@@ -28,4 +56,24 @@ func newsFeed(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+}
+
+func downloadArticles(ctx context.Context, c chan<- articleResult, d news.ArticleFeedDownloader) {
+	as, err := d.Download(ctx)
+	c <- articleResult{as, err}
+}
+
+type articleResult struct {
+	articles []news.Article
+	err      error
+}
+
+func downloadMarketing(ctx context.Context, c chan<- marketingResult, d news.MarketingFeedDownloader) {
+	ms, err := d.Download(ctx)
+	c <- marketingResult{ms, err}
+}
+
+type marketingResult struct {
+	marketing []news.Marketing
+	err       error
 }
